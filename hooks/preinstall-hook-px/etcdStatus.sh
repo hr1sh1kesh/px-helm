43eef63d234bc4e355ab6de53ae7ffd1da0a8d07
hr1sh1kesh <hr1sh1kesh.deodhar@outlook.com>
2018-02-09 11:50:49 +0530

    Updates to the hook restart policy

--- hooks/preinstall-hook-px/etcdStatus.sh
+++ hooks/preinstall-hook-px/etcdStatus.sh
@@ -3,15 +3,16 @@
 # set -x
 echo "Initializing..."
 svcname=$1
-echo $svcname
 etcdURL=$(echo "$svcname" | awk -F: '{ st = index($0,":");print substr($0,st+1)}')
-echo $etcdURL
+
+echo "Verifying if the provided etcd url is accessible: $etcdURL"
 
 response=$(curl --write-out %{http_code} --silent --output /dev/null "$etcdURL/version")
-echo $response
+echo "Response Code: $response"
 
 if [[ "$response" != 200 ]]
 then
-    echo "Provided etcd url is not reachable. Exiting.." > /dev/termination-log
+    echo "Incorrect ETCD URL provided. It is either not reachable or is incorrect..."
+    echo "Provided etcd url is not reachable or is incorrect. Exiting.." > /dev/termination-log
     exit 1
 fi 
